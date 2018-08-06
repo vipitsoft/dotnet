9dc5b7a4fc4eea1f761c13f46a9eaa5dc5883911
vipitsoft <vipitsoft@163.com>
2018-08-06 10:06:14 +0800

    初始化

---
+++ NLogTest/NLogTest/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using NLogTest.Models;
+
+namespace NLogTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+        public IActionResult Index()
+        {
+            logger.Error("测试错误信息!");
+            logger.Info("测试信息！");
+            logger.Warn("测试警告！");
+            logger.Debug("测试调试信息！");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
